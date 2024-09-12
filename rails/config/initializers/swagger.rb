3318e25f7cf5494bc4d4e92a5a0bcdcf205e1556
Andrew Apodaca <andy.apod@gmail.com>
2024-09-12 15:39:01 -0700

    adds requirements for running swagger UI locally

--- rails/config/initializers/swagger.rb
+++ rails/config/initializers/swagger.rb
@@ -1,2 +1,2 @@
-# GrapeSwaggerRails.options.url      = '/api'
-# GrapeSwaggerRails.options.app_url  = 'http://localhost:3010/swagger/w'
+GrapeSwaggerRails.options.url      = '/swagger_doc'
+GrapeSwaggerRails.options.app_url  = ''
