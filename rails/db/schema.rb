4c5a08ec1dc5da5c08d872ba7bb429f96ec4e7d2
Andrew Apodaca <andy.apod@gmail.com>
2024-09-12 18:49:58 -0700

    finish db migration

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_223112) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_204921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,5 +41,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_30_223112) do
     t.string "display_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
