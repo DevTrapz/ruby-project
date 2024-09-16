5af5f849a8b8a24a304b12d066f1ba77fcf6b5f9
Raoul Apodaca <raoul.fresno@gmail.com>
2024-09-16 15:37:17 -0700

    Added grades by roa

--- rails/app/models/schoology_record.rb
+++ rails/app/models/schoology_record.rb
@@ -1,6 +1,44 @@
 class SchoologyRecord < ApplicationRecord
-  belongs_to :student
+  belongs_to :student, -> {select("*,first_name || ' ' || last_name || ' (' || username || ')' as name")}, class_name: "Student", foreign_key: "student_id"
   belongs_to :course
+
+  def get_roa roa_number=1
+    roa_headers = grades.to_h.keys.select{|k| k.include?("ROA #{roa_number}")}
+    grades.select{|header,value| roa_headers.include?(header)}.to_h
+  end
+
+  def course_attendance_grade roa_number=1, days_in_roa = 8
+    assignments = get_roa(roa_number).select{|header,value| header.include?("Grading Category: ")}
+    completed = assignments.values.map {|value| (value.to_i > 0) ? 1 : 0 }.sum
+    percentage = (completed.to_f/assignments.count)
+    {
+      completed_assignments: completed,
+      total_assignments: assignments.count,
+      percentage: percentage.round(3),
+      days: (days_in_roa*percentage).round
+    }
+  end
+
+  def course_grade roa_number=1
+    (get_roa(roa_number).find{|header,value| header.include?("- Category Score")}[1].to_f/100).round(3)
+  end
+
+  def self.get_grades schoology_records, roa_number
+    schoology_records.includes(:student,:course).map do |a| 
+      [a.student.name, 
+        { 
+          course: a.course.name,
+          grade: a.course_grade(roa_number),
+          attendance: a.course_attendance_grade(roa_number),
+        }
+      ]
+    end.group_by do |a| 
+      a[0]
+    end.map do |k,v| 
+      [k, v.map{|a| a[1]}]
+    end.to_h
+  end
+
   def self.import_csv course_filename, teacher_email
     require 'csv'
     headers = CSV.read(course_filename,headers:true).headers
