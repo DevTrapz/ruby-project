151fe213a1d51e6a64ad1f4d0f3ebef41470d56b
Andrew Apodaca <andy.apod@gmail.com>
2024-09-27 01:13:52 -0700

    Rspec test for schoology route

--- rails/app/api/admin/schoology.rb
+++ rails/app/api/admin/schoology.rb
@@ -5,12 +5,22 @@ module Admin
       desc "Upload gradebook csv into students, create a teacher if they don't already exist and course", consumes: ['multipart/form-data']
       params do
         requires :csv, type: File, desc: "schoology grade book csv data"
-        requires :course_name, type: String, desc: "course name"
-        optional :email, type: String, desc: "teacher email"
+        requires :teacher_id, type: Integer
+        requires :course_name, type: String
       end
-      post :upload_gradebook do
+      post "upload_gradebook/teacher/:teacher_id" do
         begin
-          SchoologyRecord.import_csv(params[:csv][:tempfile].path, params[:email], params[:course_name])
+          filepath = params[:csv][:tempfile].path
+          unless SchoologyRecord.validate_csv(filepath)
+            body({ error: "csv invalid format"})
+            return status 400
+          end
+          teacher = Teacher.where(id: params[:teacher_id]).first
+          unless teacher
+            body({ error: "teacher does not exist" })
+            return status 404        
+          end
+          SchoologyRecord.import_csv(filepath, teacher.email, params[:course_name])
           body({ success: "csv uploaded successfully"})
         rescue
           status 400
--- rails/spec/api/schoology.rb
+++ rails/spec/api/schoology.rb
@@ -3,15 +3,38 @@ require "rails_helper"
 describe Admin::Schoology, type: :request do
   
   context 'POST /schoology/upload_gradebook' do
-    before :each  do
-      @file = fixture_file_upload('gradebook-export-SAMPLE.csv', 'multipart/form-data')
-
-      @params = {csv: @file, course_name: "pre-algebra", email: "issac.newton@math.edu"}
-    end
-    it 'uploads gradebook csv' do
-      post '/api/schoology/upload_gradebook', params: @params
+    it 'uploads csv' do
+      response = upload_csv
       expect(response.status).to eq(201)
-      expect(response.content_type).to include('application/json')
+      check_payload response, "success"
+    end
+    it 'uploads invalid csv format' do
+      response = upload_csv "bad gradebook.csv"
+      expect(response.status).to eq(400)
+      check_payload response, "error"
+    end
+    it 'uploads invalid csv format' do
+      response = upload_csv "gradebook.csv", false
+      expect(response.status).to eq(404)
+      check_payload response, "error"
+    end
+  end
+  
+  def upload_csv csv="gradebook.csv", use_teacher=true
+    file = fixture_file_upload(csv, 'multipart/form-data')
+    teacher = Teacher.create(display_name: "Issac", email: "issac.newton@math.edu")
+    teacher_id = use_teacher ? teacher.id : (teacher.id + 1)
+    params = {csv: file, course_name: "pre-algebra"}
+    post "/api/schoology/upload_gradebook/teacher/#{teacher_id}", params: params
+    response
+  end
+
+  def check_payload response, payload_type
+    case payload_type
+    when "error"
+      expect(JSON.parse(response.body).keys.first).to eq("error")
+    when "success"
+      expect(JSON.parse(response.body).keys.first).to eq("success")
     end
   end
 end
